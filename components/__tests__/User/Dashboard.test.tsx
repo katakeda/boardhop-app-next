539f81dc7c5c35db20d9a5887f5acad5ed53e7e4
Kaitaku Takeda <katakeda32@gmail.com>
2022-03-15 00:53:43 -0700

    Add tests

---
+++ components/__tests__/User/Dashboard.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { AuthProvider } from '../../../contexts/AuthContext';
+import { User } from '../../../types/common';
+import * as UserUtil from '../../../utils/user';
+import { Dashboard } from '../../User/Dashboard';
+
+const MOCK_USER: User = {
+  id: 'test-id',
+  email: 'test@test.com',
+  firstName: 'test',
+  lastName: 'test',
+  avatarUrl: '',
+}
+
+const renderWithAuthContext = (children: React.ReactElement) => {
+  render(<AuthProvider>{children}</AuthProvider>);
+}
+
+describe('User.Dashboard component', () => {
+  beforeEach(jest.resetAllMocks);
+
+  it('should be loading when user is empty', async () => {
+    jest.spyOn(UserUtil, 'getUser').mockResolvedValue({ user: MOCK_USER, error: null });
+    renderWithAuthContext(<Dashboard />);
+
+    expect(screen.getByText('Loading')).toBeInTheDocument();
+  })
+
+  it('should render a user email', async () => {
+    jest.spyOn(UserUtil, 'getUser').mockResolvedValue({ user: MOCK_USER, error: null });
+    renderWithAuthContext(<Dashboard />);
+
+    await waitFor(() => {
+      expect(UserUtil.getUser).toBeCalledTimes(1);
+    });
+
+    expect(screen.getByText('User: test@test.com')).toBeInTheDocument();
+  })
+})
