615c142412b82842271c105ece0324d6b1b64328
Kaitaku Takeda <katakeda32@gmail.com>
2022-05-04 21:35:52 -0700

    Update layout

--- components/Common/DefaultLayout.tsx
+++ components/Common/DefaultLayout.tsx
@@ -4,18 +4,24 @@ import { AuthProvider } from '../../contexts/AuthContext';
 import { DefaultFooter } from '../Footer/DefaultFooter';
 import { DefaultHeader } from '../Header/DefaultHeader';
 
-interface DefaultLayoutProps { }
+interface DefaultLayoutProps {}
 
 export const DefaultLayout: React.FC<DefaultLayoutProps> = ({ children }) => {
   return (
     <AppProvider>
       <AuthProvider>
-        <div className="flex flex-col h-screen">
-          <header className="h-header"><DefaultHeader /></header>
-          <main className="h-main">{children}</main>
-          <footer className="h-footer"><DefaultFooter /></footer>
+        <div className="flex flex-col">
+          <header className="h-header">
+            <DefaultHeader />
+          </header>
+          <main className="h-main">
+            {children}
+          </main>
+          <footer className="h-footer">
+            <DefaultFooter />
+          </footer>
         </div>
       </AuthProvider>
     </AppProvider>
   );
-}
+};
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,27 +2,41 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-interface HomeProps {
-
-}
+interface HomeProps {}
 
 export const Home: React.FC<HomeProps> = () => {
   return (
-    <div className="flex justify-center items-center bg-gray-100 h-full">
-      <div className="flex flex-col justify-center items-center gap-10 px-3 w-full h-full">
+    <div className="flex justify-center items-center h-main">
+      <div className="flex flex-col gap-10 w-full h-full">
         <Link href="/posts?type=surfboards" passHref>
           <span className="flex justify-center items-center relative">
-            <p className="flex justify-center items-center absolute inset-0 text-3xl text-gray-100 z-10">Surfboards</p>
-            <Image className="rounded-3xl" src="/img/home-surfing.jpg" alt="home-surfing" width="250" height="250" />
+            <p className="flex justify-center items-center absolute inset-0 text-3xl text-gray-100 z-10">
+              Surfboards
+            </p>
+            <Image
+              className="rounded-3xl"
+              src="/img/home-surfing.jpg"
+              alt="home-surfing"
+              width="250"
+              height="250"
+            />
           </span>
         </Link>
         <Link href="/posts?type=snowboards" passHref>
           <span className="flex justify-center items-center relative">
-            <p className="flex justify-center items-center absolute inset-0 text-3xl text-gray-100 z-10">Snowboards</p>
-            <Image className="rounded-xl w-full" src="/img/home-snowboarding.jpg" alt="home-snowboarding" width="250" height="250" />
+            <p className="flex justify-center items-center absolute inset-0 text-3xl text-gray-100 z-10">
+              Snowboards
+            </p>
+            <Image
+              className="rounded-xl w-full"
+              src="/img/home-snowboarding.jpg"
+              alt="home-snowboarding"
+              width="250"
+              height="250"
+            />
           </span>
         </Link>
       </div>
     </div>
   );
-}
+};
