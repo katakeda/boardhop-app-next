8b42f40eaa55b312c3886e36eafc495dc6aa4c0a
Kaitaku Takeda <katakeda32@gmail.com>
2022-05-25 00:47:26 -0700

    Add handler for creating message

--- components/User/ReceiptDetail.tsx
+++ components/User/ReceiptDetail.tsx
@@ -1,25 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { UserIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { Order, OrderStatus } from '../../types/common';
 import { currencyFormat, sortByNewest } from '../../utils/common';
+import { API_ENDPOINT } from '../../utils/constants';
 import { getOrder } from '../../utils/frontend/orders';
 import { DefaultLoading } from '../Common/DefaultLoading';
 
 export const ReceiptDetail: React.FC = () => {
   const router = useRouter();
+  const messageRef = useRef<HTMLTextAreaElement>(null);
   const [order, setOrder] = useState<Order | null>(null);
+  const [msgError, setMsgError] = useState<string | null>(null);
   const { id } = router.query;
 
   useEffect(() => {
-    (async () => {
-      const { order, error } = await getOrder(id);
-      if (!error && order) {
-        setOrder(order);
-      }
-    })();
+    updateOrder(id);
   }, [id]);
+
+  const updateOrder = async (id: string | Array<string> | undefined) => {
+    const { order, error } = await getOrder(id);
+    if (!error && order) {
+      setOrder(order);
+    }
+  };
+
+  const handleMessageSubmit = async () => {
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        orderId: order?.id,
+        message: messageRef.current?.value,
+      }),
+    };
+
+    const response = await fetch(`${API_ENDPOINT}/messages`, options);
+    if (response.status >= 300) {
+      return setMsgError('メッセージの送信中にエラーが発生しました');
+    }
+
+    updateOrder(id);
+  };
 
   if (!order) {
     return <DefaultLoading />;
@@ -64,12 +87,17 @@ export const ReceiptDetail: React.FC = () => {
         </div>
       </div>
       <div className="flex flex-col w-96 mb-16">
+        {msgError && <div className="w-full p-2 text-red-500">{msgError}</div>}
         <textarea
           className="w-full p-2 shadow-md rounded-md border border-gray-100"
-          rows={3}
           placeholder="メッセージを書き込む"
+          rows={3}
+          ref={messageRef}
         ></textarea>
-        <button className="inline-flex items-center justify-center w-full p-2 mt-3 rounded-md shadow-sm text-white bg-primary-500 disabled:opacity-50">
+        <button
+          className="inline-flex items-center justify-center w-full p-2 mt-3 rounded-md shadow-sm text-white bg-primary-500 disabled:opacity-50"
+          onClick={handleMessageSubmit}
+        >
           送信
         </button>
         {order.messages.sort(sortByNewest).map((msgObj) => (
---
+++ pages/api/messages/index.ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Message, ResponseData } from '../../../types/common';
+import { methodNotAllowed } from '../../../utils/backend/http';
+
+type PostData = {
+  message: Message | null;
+} & ResponseData;
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) => {
+  switch (req.method) {
+    case 'POST':
+      return handlePost(req, res);
+    default:
+      return methodNotAllowed(req, res);
+  }
+};
+
+const handlePost = async (
+  req: NextApiRequest,
+  res: NextApiResponse<PostData>
+) => {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(req.body),
+  };
+
+  const response = await fetch(
+    `${process.env.BACKEND_API_ENDPOINT}/messages`,
+    options
+  );
+
+  if (response.status >= 400) {
+    return res
+      .status(response.status)
+      .json({ message: null, error: response.statusText });
+  }
+
+  const message = await response.json();
+
+  return res.status(200).json({ message });
+};
+
+export default handler;
