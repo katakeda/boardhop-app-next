9fb1ff0ab274479ce60504c98b693b143ac36d42
Kaitaku Takeda <katakeda32@gmail.com>
2022-06-15 23:42:46 -0700

    Add login middleware for /posts/new

--- pages/posts/new/index.tsx
+++ pages/posts/new/index.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from 'react';
-import { DefaultLayout } from '../../components/Common/DefaultLayout';
-import { PostNew } from '../../components/PostNew/PostNew';
-import type { NextPageWithLayout } from '../../types/common';
+import { DefaultLayout } from '../../../components/Common/DefaultLayout';
+import { PostNew } from '../../../components/PostNew/PostNew';
+import type { NextPageWithLayout } from '../../../types/common';
 
 const NewPostPage: NextPageWithLayout = () => {
   return (
