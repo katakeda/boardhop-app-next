4d561143f0de1f3b6c61a9755d27bde1f67a6d6b
Kaitaku Takeda <katakeda32@gmail.com>
2022-06-30 22:48:38 -0700

    Fix style

--- components/PostsSearch/Posts.tsx
+++ components/PostsSearch/Posts.tsx
@@ -8,19 +8,19 @@ export const Posts: React.FC<{
 }> = ({ sortItem }) => {
   const posts = usePostsContext((value) => value.state.posts);
 
+  if (posts.length <= 0) {
+    return (
+      <div className="w-full text-center text-gray-500">
+        <p>検索結果が見つかりませんでした</p>
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-2 gap-4 w-full">
-      {posts.length > 0 ? (
-        posts
-          .sort(sortItem.sort)
-          .map((post: PostType, index: number) => (
-            <Post key={index} post={post} />
-          ))
-      ) : (
-        <div className="w-full text-center text-gray-500">
-          <p>検索結果が見つかりませんでした</p>
-        </div>
-      )}
+      {posts.sort(sortItem.sort).map((post: PostType, index: number) => (
+        <Post key={index} post={post} />
+      ))}
     </div>
   );
 };
